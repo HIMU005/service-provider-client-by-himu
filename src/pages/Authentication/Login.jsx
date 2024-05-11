36868037ddd1c1d436b84b721876a276df0e5845
HIMU005 <hashanuzzaman99@gamil.com>
2024-05-12 00:28:46 +0600

    design the form for the login and registration

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -1,10 +1,38 @@
 
 const Login = () => {
-    return (
-        <div>
+    const handleSignIn = async e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
 
-        </div>
-    );
+        console.log(email, password);
+    }
+
+
+    return (
+        <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+            <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white"> SignUp Now</h2>
+
+            <form onSubmit={handleSignIn}>
+                <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+
+                    <div>
+                        <label className="text-gray-700 dark:text-gray-200" htmlFor="emailAddress">Email Address <span className="text-red-600">*</span></label>
+                        <input id="emailAddress" type="email" name="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required />
+                    </div>
+
+                    <div>
+                        <label className="text-gray-700 dark:text-gray-200" htmlFor="password">Password <span className="text-red-600">*</span></label>
+                        <input id="password" type="password" name="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required />
+                    </div>
+                </div>
+
+                <div className="flex justify-end mt-6">
+                    <input type="submit" value="Sign In" className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600" />
+                </div>
+            </form>
+        </section>)
 };
 
 export default Login;
--- src/pages/Authentication/Register.jsx
+++ src/pages/Authentication/Register.jsx
@@ -1,9 +1,47 @@
 
 const Register = () => {
-    return (
-        <div>
+    const handleRegister = async e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const photo = form.photo.value;
+        const password = form.password.value;
 
-        </div>
+        console.log(name, email, photo, password);
+    }
+    return (
+        <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+            <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white"> SignUp Now</h2>
+
+            <form onSubmit={handleRegister}>
+                <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                    <div>
+                        <label className="text-gray-700 dark:text-gray-200" htmlFor="username">Username</label>
+                        <input id="username" type="text" name="name" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                    </div>
+
+                    <div>
+                        <label className="text-gray-700 dark:text-gray-200" htmlFor="emailAddress">Email Address <span className="text-red-600">*</span></label>
+                        <input id="emailAddress" type="email" name="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required />
+                    </div>
+
+                    <div>
+                        <label className="text-gray-700 dark:text-gray-200" htmlFor="photo">PhotoUrl</label>
+                        <input id="photo" type="text" name="photo" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                    </div>
+
+                    <div>
+                        <label className="text-gray-700 dark:text-gray-200" htmlFor="password">Password <span className="text-red-600">*</span></label>
+                        <input id="password" type="password" name="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required />
+                    </div>
+                </div>
+
+                <div className="flex justify-end mt-6">
+                    <input type="submit" value="Register" className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600" />
+                </div>
+            </form>
+        </section>
     );
 };
 
